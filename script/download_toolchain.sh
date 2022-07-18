079afc3216df36bf3aea58536558c799acb74a73
alter-xp <xp56@linux.alibaba.com>
2022-07-18 17:48:46 +0800

    remvoe useless file

--- script/download_toolchain.sh
+++ script/download_toolchain.sh
@@ -3,7 +3,6 @@
 ROOT_PATH=$PWD
 GCC_LINK=https://github.com/T-head-Semi/csi-nn2/releases/download/v1.12/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.2.5-20220323.tar.gz
 TAR_FILE=$ROOT_PATH/tools/Xuantie-900-gcc-linux-toolchain.tar.gz
-COLOR="\\E[5;33m"
 
 # create dir
 GCC_PATH=tools/gcc-toolchain
@@ -19,8 +18,9 @@ if [ -f "$TAR_FILE" ]; then
 	if [ ! -d $ROOT_PATH/$GCC_PATH/bin ]; then
 		tar -zxvf $TAR_FILE -C $ROOT_PATH/$GCC_PATH --strip-components 1
 	fi
+	rm -rf $TAR_FILE
 fi
 
-echo -e  $COLOR"Download toolchain in $ROOT_PATH/tools/"
-echo -e $COLOR"Set env variable:
+echo "Download toolchain in $ROOT_PATH/tools/"
+echo "Set env variable:
 	export PATH=$ROOT_PATH/$GCC_PATH/bin:$ \bPATH"
