c7b209761c8c5917de0159d6ac671d07d28e08f3
alter-xp <xp56@linux.alibaba.com>
2022-07-18 20:05:54 +0800

    remvoe useless file (#5)

--- script/download_qemu.sh
+++ script/download_qemu.sh
@@ -3,7 +3,6 @@
 ROOT_PATH=$PWD
 QEMU_LINK=https://github.com/T-head-Semi/csi-nn2/releases/download/v1.12/xuantie-qemu-x86_64-Ubuntu-18.04-20220402-0353.tar.gz
 TAR_FILE=$ROOT_PATH/tools/Xuantie-Qemu-x86_64-Ubuntu-18.04.tar.gz
-COLOR="\\E[5;33m"
 
 # create dir
 QEMU_PATH=tools/qemu
@@ -18,9 +17,18 @@ fi
 if [ -f "$TAR_FILE" ]; then
 	if [ ! -d $ROOT_PATH/$QEMU_PATH/bin ]; then
 		tar -zxvf $TAR_FILE -C $ROOT_PATH/$QEMU_PATH --strip-components 1
+		echo "remove $TAR_FILE"
 	fi
+	rm -f $TAR_FILE
 fi
 
-echo -e  $COLOR"Download toolchain in $ROOT_PATH/tools/"
-echo -e $COLOR"Set env variable:
+if [ -d "$ROOT_PATH/$QEMU_PATH/bin" ]; then
+	echo "remove usless file"
+	ls $ROOT_PATH/$QEMU_PATH/bin |grep -v qemu-riscv64 |xargs -i rm -rf $ROOT_PATH/$QEMU_PATH/bin/{}
+	ls $ROOT_PATH/$QEMU_PATH |grep -v bin |xargs -i rm -rf $ROOT_PATH/$QEMU_PATH/{}
+fi
+
+
+echo "Download toolchain in $ROOT_PATH/tools/"
+echo "Set env variable:
 	export PATH=$ROOT_PATH/$QEMU_PATH/bin:$ \bPATH"
